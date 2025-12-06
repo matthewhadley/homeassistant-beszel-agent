3c90fa395716e72324022c2412c32c1cc15b6314
matthewhadley <matthew.hadley@gmail.com>
2025-12-05 21:11:42 -0600

    feat: support optional beszel config

--- beszel-agent/run.sh
+++ beszel-agent/run.sh
@@ -10,4 +10,17 @@ declare __BASHIO_LOG_TIMESTAMP="%Y-%m-%d %T"
 bashio::log.info "Beszel-Agent Home Assistant Add-On $VERSION"
 bashio::log.info "Beszel-Agent $BESZEL_AGENT_VERSION"
 
-PORT=45876 KEY="$PUBLIC_KEY" ./beszel-agent
+# Optional HUB_URL
+if bashio::config.has_value 'hub_url'; then
+    export HUB_URL="$(bashio::config 'hub_url')"
+fi
+
+# Optional TOKEN
+if bashio::config.has_value 'token'; then
+    export TOKEN="$(bashio::config 'token')"
+fi
+
+PORT="$(bashio::config 'port')"
+
+# Run agent
+PORT="${PORT:-45876}" KEY="$PUBLIC_KEY" ./beszel-agent
