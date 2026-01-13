41319a030fcfcec86a45259456ca1c1ce5590624
matthewhadley <matthew.hadley@gmail.com>
2026-01-12 21:34:14 -0600

    fix: remove old version reference

--- beszel-agent/run.sh
+++ beszel-agent/run.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/with-contenv bashio
 
-VERSION=$(cat VERSION)
 BESZEL_AGENT_VERSION=$(cat BESZEL_AGENT_VERSION)
 PUBLIC_KEY="$(bashio::config 'public_key')"
 
 # add date to default bashio log timestamp
 declare __BASHIO_LOG_TIMESTAMP="%Y-%m-%d %T"
 
-# bashio::log.info "Beszel-Agent Home Assistant Add-On $VERSION"
 bashio::log.info "Beszel-Agent $BESZEL_AGENT_VERSION"
 
 # Optional HUB_URL
